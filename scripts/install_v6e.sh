92b57d978119b12bb2d81fb72a909bf06e9eb4f2
Hanhong Zhao <zhh24@mit.edu>
2025-10-08 21:21:45 +0000

    faithfully set v6 env

--- scripts/install_v6e.sh
+++ scripts/install_v6e.sh
@@ -4,5 +4,5 @@ cd
 gsutil -m cp -r gs://kmh-gcp-us-east1/hanhong/v6_wheels.tar.gz ./wheels.tar.gz
 tar -xvf wheels.tar.gz
 rm -rf .local || true
-pip install --no-index --find-links=wheels wheels/*.whl --no-deps --force-reinstall
+pip install --no-index --find-links=wheels wheels/*.whl --no-deps --force-reinstall --no-warn-script-location
 rm -rf wheels wheels.tar.gz
